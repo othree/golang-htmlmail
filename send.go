d95380392f8a5ef84820c5e06bb1cb1edd55d2e9
othree <othree@gmail.com>
2015-08-14 15:41:49 +0800

    First commit

---
+++ send.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"crypto/tls"
+	"io/ioutil"
+	"gopkg.in/gomail.v1"
+	"github.com/vaughan0/go-ini"
+)
+
+func main() {
+	config, err := ini.LoadFile("config.ini")
+	if err != nil {
+		panic("Config file not loaded.")
+	}
+	USER, ok := config.Get("gmail", "user")
+  if !ok {
+    panic("Gmail user not set.")
+  }
+  PASS, ok := config.Get("gmail", "pass")
+  if !ok {
+    panic("Gmail pass not set.")
+  }
+
+
+	msg := gomail.NewMessage()
+	msg.SetHeader("From", USER + "@gmail.com")
+	msg.SetHeader("To", USER + "@gmail.com")
+
+	title, err := ioutil.ReadFile("title")
+	if err != nil {
+		panic(err)
+	}
+	html, err := ioutil.ReadFile("body.html")
+	if err != nil {
+		panic(err)
+	}
+	msg.SetHeader("Subject", string(title))
+	msg.SetBody("text/html", string(html))
+
+	// Send the email to Bob
+	mailer := gomail.NewMailer("smtp.gmail.com", USER, PASS, 587, gomail.SetTLSConfig(&tls.Config{InsecureSkipVerify: true}))
+	if err := mailer.Send(msg); err != nil {
+		panic(err)
+	}
+}
